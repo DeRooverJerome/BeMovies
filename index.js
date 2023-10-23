d385a473ee036c39d8f15c7d79ad5250285229c7
DeRooverJerome <derooverjdr@gmail.com>
2023-10-23 17:03:40 +0200

    first day

---
+++ index.js
@@ -0,0 +1,11 @@
+// GOOD LUCK MATE ! :D YOU CAN DO IT !
+
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  centeredSlides: false,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
