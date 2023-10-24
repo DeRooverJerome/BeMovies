b6f86d14f279c741a1543a330b43104f7a77ea84
Jerome <derooverjdr@gmail.com>
2023-10-24 09:21:53 +0200

    anchor tag changes

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ let swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-prev",
   },
 });
+
+let swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  centeredSlides: false,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-next-second",
+    prevEl: ".swiper-prev-second",
+  },
+});
