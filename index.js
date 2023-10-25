bd6886d3de5229696ccdf7400996bc8d1b083c0f
MateoDedi <mateod@live.com>
2023-10-25 16:49:08 +0200

    changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Wrap your code in a DOMContentLoaded event listener
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize Swiper
   console.log("Initializing Swiper");
@@ -77,15 +76,26 @@ document.addEventListener("DOMContentLoaded", function () {
   let searchInput = document.getElementById("inputSearch");
   let searchResult = "";
 
-  searchInput.addEventListener("input", (e) => {
-    searchResult = e.target.value;
-    console.log("Search input changed:", searchResult);
-    fetchMovies(); // Call fetchMovies whenever the input changes
+  // Listen for "keydown" event
+  searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      // Only fetch when the Enter key is pressed
+      searchResult = e.target.value;
+      console.log("Search input submitted:", searchResult);
+      fetchMovies();
+    }
+  });
+
+  // Listen for click event on the search button
+  document.querySelector(".validateSearch").addEventListener("click", () => {
+    searchResult = searchInput.value;
+    console.log("Search button clicked:", searchResult);
+    fetchMovies();
   });
 
   async function fetchMovies() {
     const apiKey = 'c84fa46197059b44b8001782df185e79';
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchResult}&language=fr-FR`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchResult}&language=en-EN`;
     console.log("Fetching movies with URL:", url);
 
     try {
@@ -110,15 +120,15 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Displaying results");
     const message = document.querySelector('.message');
     const moviePostersContainer = document.getElementById('moviePosters');
-    
+
     // Verify the existence of moviePostersContainer before modifying it
     if (moviePostersContainer) {
       moviePostersContainer.innerHTML = ''; // Clear previous posters
-  
+
       if (results.length > 0) {
         message.textContent = `Results for '${searchResult}'`;
         message.style.opacity = 1;
-  
+
         // Iterate through the search results and add poster images to the swiper
         results.forEach((movie) => {
           if (movie.poster_path) {
@@ -131,9 +141,8 @@ document.addEventListener("DOMContentLoaded", function () {
             moviePostersContainer.appendChild(swiperSlide);
           }
         });
-  
+
         // After modifying the content, update the swiper to reflect the changes
-        swiper.update();
       } else {
         message.textContent = `No results for '${searchResult}'`;
         message.style.opacity = 1;
@@ -147,4 +156,163 @@ document.addEventListener("DOMContentLoaded", function () {
     message.textContent = `No results for '${searchResult}'`;
     message.style.opacity = 1;
   }
+
+  // Fetch latest movies for mySwiper2
+  async function fetchLatestMovies() {
+    const apiKey = 'c84fa46197059b44b8001782df185e79';
+    const today = new Date();
+    const oneMonthAgo = new Date();
+    oneMonthAgo.setMonth(today.getMonth() - 1);
+
+    const formattedToday = today.toISOString().split('T')[0];
+    const formattedOneMonthAgo = oneMonthAgo.toISOString().split('T')[0];
+
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_date.gte=${formattedOneMonthAgo}&primary_release_date.lte=${formattedToday}&language=en-EN`;
+    console.log("Fetching latest movies with URL:", url);
+
+    try {
+      const response = await fetch(url);
+      console.log("API Response:", response);
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("API Data:", data);
+        displayLatestMovies(data.results);
+      } else {
+        console.log("API request failed");
+        displayNoLatestMovies();
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      displayNoLatestMovies();
+    }
+  }
+
+  // Display latest movies in mySwiper2
+  function displayLatestMovies(results) {
+    console.log("Displaying latest movies");
+    const message = document.querySelector('.message');
+    const latestMoviesContainer = document.querySelector('.swiper.mySwiper2 .swiper-wrapper');
+
+    // Verify the existence of latestMoviesContainer before modifying it
+    if (latestMoviesContainer) {
+      latestMoviesContainer.innerHTML = ''; // Clear previous posters
+
+      if (results.length > 0) {
+        message.textContent = "Latest Releases";
+        message.style.opacity = 1;
+
+        // Iterate through the latest movie results and add poster images to mySwiper2
+        results.forEach((movie) => {
+          if (movie.poster_path) {
+            const posterURL = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            const posterImage = document.createElement('img');
+            posterImage.src = posterURL;
+            const swiperSlide = document.createElement('div');
+            swiperSlide.classList.add('swiper-slide');
+            swiperSlide.appendChild(posterImage);
+            latestMoviesContainer.appendChild(swiperSlide);
+          }
+        });
+      } else {
+        message.textContent = "No latest releases found";
+        message.style.opacity = 1;
+      }
+    }
+  }
+
+  function displayNoLatestMovies() {
+    console.log("Displaying no latest releases");
+    const message = document.querySelector('.message');
+    message.textContent = "No latest releases found";
+    message.style.opacity = 1;
+  }
+
+  // Add an event listener to fetch latest movies when the DOM content is loaded
+  fetchLatestMovies();
+
+
+  // MOVIE GENRES
+
+  // Add an event listener to the .sortMovies section to handle list item clicks
+  const sortList = document.querySelector('.sortMovies .list ul');
+  const messageGenres = document.querySelector('.messageGenres p');
+
+  sortList.addEventListener('click', (e) => {
+    const clickedItem = e.target;
+    if (clickedItem.tagName === 'LI') {
+      // Remove the .redBgMovies class from all list items
+      const listItems = sortList.querySelectorAll('li');
+      listItems.forEach((item) => {
+        item.classList.remove('redBgMovies');
+        fetchMoviesByGenre() /////////////////////////////
+      });
+
+      // Add the .redBgMovies class to the clicked list item
+      clickedItem.classList.add('redBgMovies');
+
+      // Set the messageGenres content based on the selected genre
+      messageGenres.textContent = clickedItem.textContent;
+    }
+
+    
+  });
+  
+  async function fetchMoviesByGenre() {
+    
+    const apiKey = 'c84fa46197059b44b8001782df185e79';
+
+    const url = `https://api.themoviedb.org/3/${messageGenres}/movie/list`;
+    console.log("Fetching movies genre with URL:", url);
+
+    try {
+      const response = await fetch(url);
+      console.log("API Response:", response);
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("API Data:", data);
+        fetchMoviesByGenre(data.results);
+      } else {
+        console.log("API request failed");
+        displayNoMoviesByGenre();
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      displayNoMoviesByGenre();
+    }
+
+    if (latestMoviesContainer) {
+      latestMoviesContainer.innerHTML = ''; // Clear previous posters
+
+      if (results.length > 0) {
+        message.textContent = "Latest Releases";
+        message.style.opacity = 1;
+
+        // Iterate through the latest movie results and add poster images to mySwiper2
+        results.forEach((movie) => {
+          if (movie.poster_path) {
+            const posterURL = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            const posterImage = document.createElement('img');
+            posterImage.src = posterURL;
+            const swiperSlide = document.createElement('div');
+            swiperSlide.classList.add('swiper-slide');
+            swiperSlide.appendChild(posterImage);
+            latestMoviesContainer.appendChild(swiperSlide);
+          }
+
+        });
+        
+      } else {
+        message.textContent = "No latest releases found";
+        message.style.opacity = 1;
+      }
+    }
+  }
+
+  function displayNoMoviesByGenre() {
+    console.log("Displaying no movies by genre");
+    message.style.opacity = 1;
+  }
+  
 });
